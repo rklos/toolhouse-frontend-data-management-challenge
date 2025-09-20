0ed7089e17532fb8cde662e0a8f76a25cc342502
Radosław Kłos <radoslaw@klos.dev>
2025-09-20 12:11:30 +0200

    Enable api mocking

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableApiMocking() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./api/__mocks__/browser');
+    await worker.start();
+  }
+}
+
+enableApiMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+});
