55b8beebe29825df8146ede1ebbde26d0dc704c0
Radosław Kłos <radoslaw@klos.dev>
2025-09-21 20:37:49 +0200

    Add active state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,17 +2,20 @@ import cn from 'classnames';
 
 interface Props {
   disabled?: boolean;
+  active?: boolean;
   children: React.ReactNode;
   onClick: () => void;
 }
 
-export function Button({ disabled, children, onClick }: Props) {
+export function Button({ disabled, active, children, onClick }: Props) {
   const classNames = cn(
     'border border-gray-300 text-xs px-2 py-1 rounded-md',
     {
-      'hover:bg-gray-700': !disabled,
-      'cursor-pointer': !disabled,
+      'hover:bg-gray-700': !disabled && !active,
+      'cursor-pointer': !disabled && !active,
       'opacity-10': disabled,
+      'bg-gray-300': active,
+      'text-gray-900': active,
     },
   );
 
