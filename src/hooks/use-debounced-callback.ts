26520880fb9eea18b071d3f6e3fccec486e3c660
Radosław Kłos <radoslaw@klos.dev>
2025-09-21 22:39:13 +0200

    Add useDebouncedCallback hook

---
+++ src/hooks/use-debounced-callback.ts
@@ -0,0 +1,14 @@
+import { useRef } from 'react';
+
+export function useDebouncedCallback<T extends (...args: never[]) => void>(callback: T, delay: number) {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  return (...args: Parameters<T>) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
