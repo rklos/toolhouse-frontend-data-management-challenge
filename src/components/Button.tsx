4acda3d86155d7d2e78e3a6bcf69b24f97b07f33
Radosław Kłos <radoslaw@klos.dev>
2025-09-21 17:36:21 +0200

    Better styling for disabled Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,10 +8,11 @@ interface Props {
 
 export function Button({ disabled, children, onClick }: Props) {
   const classNames = cn(
-    'border border-gray-300 text-xs px-2 py-1 rounded-md hover:bg-gray-700',
+    'border border-gray-300 text-xs px-2 py-1 rounded-md',
     {
+      'hover:bg-gray-700': !disabled,
       'cursor-pointer': !disabled,
-      'opacity-50': disabled,
+      'opacity-10': disabled,
     },
   );
 
