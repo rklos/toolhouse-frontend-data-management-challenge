367af6c4a991d528fe626d068df8355d9f73d02a
Radosław Kłos <radoslaw@klos.dev>
2025-09-22 02:31:58 +0200

    Use React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
     tailwindcss(),
   ],
   test: {
